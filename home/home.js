fa008b0116d834fd021a928950f6c070fe502b2b
Leoibarrapp <laibarra.23@est.ucab.edu.ve>
2026-03-17 19:38:52 -0400

    web icon y aviso de work in progress

--- home/home.js
+++ home/home.js
@@ -1,5 +1,5 @@
-//mueve el fondo de cursor personalizado segun la posicion del mouse
-document.addEventListener("mousemove", (e) => {
-    document.documentElement.style.setProperty("--cursor-x", `${e.clientX}px`);
-    document.documentElement.style.setProperty("--cursor-y", `${e.clientY}px`);
-});
+// //mueve el fondo de cursor personalizado segun la posicion del mouse
+// document.addEventListener("mousemove", (e) => {
+//     document.documentElement.style.setProperty("--cursor-x", `${e.clientX}px`);
+//     document.documentElement.style.setProperty("--cursor-y", `${e.clientY}px`);
+// });
