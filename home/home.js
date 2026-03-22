2875b4091c08e9c753e0d1e637bdafcfa5799be0
Leoibarrapp <leonardoibarra03@gmail.com>
2026-03-22 12:57:50 -0400

    efecto spotlight en home terminado y otros detalles esteticos

--- home/home.js
+++ home/home.js
@@ -2,10 +2,6 @@ const container = document.body;
 
 //efecto de puntos brillantes que siguen al mouse
 container.addEventListener('mousemove', (e) => {
-    const rect = container.getBoundingClientRect();
-    const x = e.clientX - rect.left;
-    const y = e.clientY - rect.top;
-
-    container.style.setProperty('--x', `${x}px`);
-    container.style.setProperty('--y', `${y}px`);
+    container.style.setProperty('--x', `${e.pageX}px`);
+    container.style.setProperty('--y', `${e.pageY}px`);
 });
